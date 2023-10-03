4deaded0ca729a25c7896f422d0d6387ff196d5c
jayantsarve26 <jayantsarve707@gmail.com>
2023-10-03 14:49:03 +0530

    install react router dom

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from "./Views/Home/Home"
-import About from "./Views/About/About"
-import Contact from "./Views/Contact/Contact"
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from "./Views/Home/Home";
+import About from "./Views/About/About";
+import Contact from "./Views/Contact/Contact";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  { 
+    path: '/about',
+    element: <About />
+  },
+  { 
+    path: '/contact',
+    element: <Contact />
+  },
+
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function App()
-{
-  const path = window.location.pathname;
-  if(path === "/")
-  {
-    return <Home />;
-  }
-  else if(path === "/about")
-  {
-    return <About />;
-  }
-  else if(path === "/contact")
-  {
-    return <Contact />;
-  }
-  else
-  {
-    return <h1>404 error code</h1>;
-  }
-}
+
 root.render(
 
   <>
- <App />
+    <RouterProvider router={router}  />
   </>
 );
 
